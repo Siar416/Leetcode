87426c51b99f08e05e02d016c23dc60555c71667
Siar <83523212+Siar416@users.noreply.github.com>
2022-06-19 12:55:09 -0400

    Time: 134 ms (8.50%), Space: 41.9 MB (87.70%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  // create a stack
+  let stack = [];
+
+  // Loop through each element in the string
+  for (let i = 0; i < s.length; i++) {
+    let char = stack[stack.length - 1];
+
+    // if you encounter a starting bracket, push it onto the stack
+    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+      stack.push(s[i]);
+
+      // Pop the opening bracket off the stack if there is a corresponding closing bracket in the string
+    } else if (
+      (char === "(" && s[i] === ")") ||
+      (char === "[" && s[i] === "]") ||
+      (char === "{" && s[i] === "}")
+    ) {
+      stack.pop();
+    } else return false;
+  }
+
+  // Check empty stack
+  return stack.length ? false : true;
+};
